fc608fedf111b7bddc43720561fbd22fd09105b3
luccui000 <luccui2k@gmail.com>
2022-12-08 21:45:35 +0700

    Change

--- app/Models/Blog.php
+++ app/Models/Blog.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Models;
+namespace Luccui\ShareModel;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
--- app/Models/User.php
+++ app/Models/User.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Models;
+namespace Luccui\ShareModel;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
